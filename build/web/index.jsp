cf7e7b867067bbbd4001f687826cd6449cd71e33
Yash Jain <Yash Jain>
2017-01-22 19:09:36 +0530

    First Commit

---
+++ build/web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Jan 9, 2017, 8:53:36 PM
+    Author     : Yash Jain
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
---
+++ src/java/p1/count_listener.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1;
+import javax.servlet.*;
+/**
+ *
+ * @author Yash Jain
+ */
+public class count_listener implements ServletContextListener  {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        
+        ServletContext sc = sce.getServletContext();
+        String count = sc.getInitParameter("count_dd");
+        hit_count c = new hit_count(Integer.parseInt(count));
+        sc.setAttribute("count_dd",c);
+        
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+    
+}
